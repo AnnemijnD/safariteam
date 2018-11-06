8366b2723809499f53f0c5b8d8dd2a2e695c1e24
Rebecca <rebeccadavidsson3@gmail.com>
2018-11-06 13:08:45 +0100

    make course dictionary

--- code/course.py
+++ code/course.py
@@ -1,3 +1,6 @@
+import csv
+import os
+
 class Course(object):
     """
     Representation of a course in CollegeClass.
@@ -10,3 +13,24 @@ class Course(object):
         self.tutorial = tutorial
         self.practicum = practicum
         self.max_students = max_students
+
+    if __name__ == "__main__":
+
+
+        course = 'vakken.csv'
+
+        with open(course) as courses:
+            courses = csv.reader(courses, delimiter=';')
+            # Skip header
+            next(courses)
+
+            course_dict = {}
+            counter = 0
+
+            # Optional code to visualize data
+            for row in courses:
+                course_dict.update({counter: row[0]})
+                counter += 1
+
+
+            print(course_dict)
