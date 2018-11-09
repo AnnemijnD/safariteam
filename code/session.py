1d14cbfdbc67b6832cd1ca603296d6124905541e
Annemijn Dijkhuis <annemijndijkhuis@mbpvanannemijn.home>
2018-11-09 18:31:15 +0100

    Werkend met session in course

--- code/session.py
+++ code/session.py
@@ -5,12 +5,12 @@ class Session():
     Representation of a class (lecture, tutorial or practicum).
     """
 
-    def __init__(self, name, class_id, type, mutual_courses, group):
+    def __init__(self, name, class_id, type, mutual_courses):
         self.name = name
         self.class_id = class_id
         self.type = type
         self.mutual_courses = mutual_courses
-        self.group = group
+        self.group = []
 
 
 if __name__ == "__main__":
