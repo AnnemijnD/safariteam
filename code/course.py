1485ee0b37f76361f768fc532148381d81495d3c
Annemijn Dijkhuis <annemijndijkhuis@wcw-stud-145-109-45-203.wireless.uva.nl>
2018-11-09 12:04:31 +0100

    course

--- code/course.py
+++ code/course.py
@@ -13,34 +13,12 @@ class Course(object):
         self.tutorial = tutorial
         self.practical = practical
         self.max_students = max_students
-<<<<<<< HEAD
         self.session_lecture = session_lecture
         self.session_tutorial = session_turorial
         self.sessoin_practical = session_practical
 
 
-    def 
 
-
-        course = 'vakken.csv'
-
-        with open(course) as courses:
-            courses = csv.reader(courses, delimiter=';')
-            # Skip header
-            next(courses)
-
-            course_dict = {}
-            counter = 0
-
-            # Optional code to visualize data
-            for row in courses:
-                course_dict.update({counter: row[0]})
-                counter += 1
-
-        # vul de hoeveelheid sessions als dat er hc's zijn
-            # return al deze sessions
-=======
->>>>>>> 9abed720723ba02b19c206b214ef018e3832cfd9
 
     # Zonder de __str__ methode krijg je dus alleen <object at position 0x107b5e4...>
     def __str__(self):
