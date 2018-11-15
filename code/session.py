487a932e849bb9161d6e584b869a986f8e036fdc
sannedonker <s.a.m.donker@hotmail.com>
2018-11-15 12:32:10 +0100

    session -> name 2

--- code/session.py
+++ code/session.py
@@ -7,7 +7,7 @@ class Session(object):
     Representation of a schedule.
     """
 
-    def __init__(self, session, type, room, timeslot, day):
+    def __init__(self, name, type, room, timeslot, day):
         self.name = name
         self.type = type
         self.room = room
@@ -18,16 +18,16 @@ class Session(object):
         """
         Enables a list of objects to be iterated one element at a time.
         """
-        return iter([self.session, self.type, self.room, self.timeslot, self.day])
+        return iter([self.name, self.type, self.room, self.timeslot, self.day])
 
     def __repr__(self):
         """
         Represents an object when it is printed.
         """
-        return str([self.session, self.type])
+        return str([self.name, self.type])
 
     def __getitem__(self, day):
         return self.day
 
     def __str__(self):
-        return f'{self.session}, {self.type}'
+        return f'{self.name}, {self.type}'
