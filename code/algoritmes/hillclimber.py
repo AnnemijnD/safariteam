3ea2ffd1410aa4e4cce228c40d6d99e8e85a972e
sannedonker <s.a.m.donker@hotmail.com>
2018-11-30 13:51:36 +0100

    overall_id officiele naam

--- code/algoritmes/hillclimber.py
+++ code/algoritmes/hillclimber.py
@@ -9,9 +9,6 @@ import switch
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import numpy as np
-from random import randint
-
 
 SESSION_LEN = 72
 LIMIT = 3000
@@ -21,75 +18,6 @@ MUTUALCOURSES = 33
 LECTURECOUNT = 39
 SLOTS = 140
 SPREADPOINTS = 440
-
-CHILDREN = 2
-
-
-def genetic_algortim(schedule1, schedule2):
-    """
-    Dit gaat ooit een genetisch algortime worden.
-
-    Begin met twee roosters combineren:
-    PSEUDO:
-    - maak lineaire lijst van beide roosters
-    -
-
-    PROBLEEM:
-    HET GAAT FOUT MET EMPTY SESSIES EN DAAR WORDT IK VERDRIETIG VAN
-    """
-    flatten1 = np.array(schedule1).flatten().tolist()
-    flatten2 = np.array(schedule2).flatten().tolist()
-
-    cycles = []
-    start_point = randint(0, SLOTS - 1)
-
-    for i in range(CHILDREN):
-        cycle_i = []
-        index = flatten1.index(flatten1[start_point])
-        in_cycle = [index]
-        counter = 0
-
-        while flatten1[start_point] not in cycle_i:
-            print(counter)
-            counter += 1
-            temp = flatten2[index]
-            cycle_i.append(temp)
-            index = flatten1.index(temp)
-            in_cycle.append(index)
-
-        cycles.append(cycle_i)
-
-        start_point = randint(0, SLOTS - 1)
-        while start_point in in_cycle:
-            start_point = randint(0, SLOTS - 1)
-
-    print(cycles)
-
-
-    #     cycle_i = []
-    #     index = np.where(flatten1 == flatten1[start_point])
-    #     in_cycle = [index]
-    #     # counter = 0
-    #     print(f"index: {index}")
-    #     print(f"start_pint {flatten1[start_point]}")
-    #     # while flatten1[start_point] not in cycle_i:
-    #         # print(counter)
-    #         # counter += 1
-    #     temp = flatten2[index]
-    #     print(f"temp: {temp}")
-    #     cycle_i.append(temp)
-    #     print(f"cycle_i {cycle_i}")
-    #     # index = flatten1.index(temp)
-    #     index = np.where(flatten1 == temp)
-    #     in_cycle.append(index)
-    #
-    #     cycles.append(cycle_i)
-    #
-    #     start_point = randint(0, SLOTS - 1)
-    #     while start_point in in_cycle:
-    #         start_point = randint(0, SLOTS - 1)
-    #
-    # print(f"cycles: {cycles}")
 SAVELIMIT = 3000
 
 
--- plan.py
+++ plan.py
@@ -61,7 +61,7 @@ class Plan():
 
         session_counter = 0
         for i in range(len(session_list)):
-            session_list[i].index = session_counter
+            session_list[i].overall_id = session_counter
             session_counter += 1
 
         # # Put every session into schedule
