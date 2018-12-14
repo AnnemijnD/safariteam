0fbcbe74d018d51c91fdabda0f838943214e6474
Rebecca <rebeccadavidsson3@gmail.com>
2018-12-14 16:28:06 +0100

    merge goed

--- plan.py
+++ plan.py
@@ -155,8 +155,10 @@ class Plan():
             # Save max points to a list
             if algorithm == "Random" or algorithm == "genetic":
                 maxpoints.append(round(points))
+                print(maxpoints)
             else:
                 maxpoints.append(round(max(points)))
+                print(maxpoints)
 
         # Save schedule with highest points
         courses_schedule, spread_points, capacity_points, lecture_points, \
@@ -275,7 +277,7 @@ class Plan():
             """
 
             plan.compare_algorithm(int(n4.get()), int(x.get()), int(n.get()),
-                                int(hc2n.get()), int(hc2x.get()),
+                                int(hc2x.get()), int(hc2n.get()),
                                 int(x2.get()), int(n2.get()), float(t1.get()),
                                 float(t2.get()), type.get(), var3.get(),
                                 var1.get(), var2.get(), var4.get())
@@ -372,7 +374,7 @@ class Plan():
         x2.insert(10,"20000")
         n2.insert(10,"1")
         t1.insert(10,"5")
-        t2.insert(10,"0.05")
+        t2.insert(10,"0.1")
         # type.insert(10,"exponential")
         x2.bind('<Return>', lambda _: printresults("sa"))
         n2.bind('<Return>', lambda _: printresults("sa"))
@@ -410,7 +412,7 @@ class Plan():
         n3.grid(row=16, column=1)
         p3.insert(10,"50")
         x3.insert(10,"10")
-        n3.insert(10,"3")
+        n3.insert(10,"1")
         t3.set('k-way')
         p3.bind('<Return>', lambda _: printresults("genetic"))
         x3.bind('<Return>', lambda _: printresults("genetic"))
